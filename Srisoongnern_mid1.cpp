02b3bf17aec9e262d43f45c0d4e8e18466b07ffc
Kidchumaiork <glaosad5@gmail.com>
2024-07-16 00:53:21 +0700

    Part2 done all Finished

--- Srisoongnern_mid1.cpp
+++ Srisoongnern_mid1.cpp
@@ -15,6 +15,7 @@ private:
     double hourWork;
 
 public:
+    double total;
     void setHourPay(double HP)
     {
         hourPay = HP;
@@ -25,18 +26,73 @@ public:
     }
     void lessThan40()
     {
-        double total = hourWork * hourPay;
-        cout << "Gross Pay = " << hourPay << "*" << hourWork << " = " << total << '\n';
+        total = hourWork * hourPay;
+        cout << "Gross Pay = " << hourPay << "*" << hourWork << " = $" << total << '\n';
     }
     void between40and60()
     {
-        double total = (40 * hourPay) + ((hourWork - 40) * (hourPay * 1.5));
-        cout << "Gross Pay = (40*" << hourPay << ") + ((" << hourWork << "-40) * (" << hourPay << "*1.5)) = " << total << '\n';
+        total = (40 * hourPay) + ((hourWork - 40) * (hourPay * 1.5));
+        cout << "Gross Pay = (40*" << hourPay << ") + ((" << hourWork << "-40) * (" << hourPay << "*1.5)) = $" << total << '\n';
     }
     void moreThan60()
     {
-        double total = (40 * hourPay) + (20 * (hourPay * 1.5)) + ((hourWork - 60) * (hourPay * 2));
-        cout << "Gross Pay = (40*" << hourPay << ") + (20 * (" << hourPay << "*1.5))" << " + ((" << hourWork << "-60) * (" << hourPay << "*2)) = " << total << '\n';
+        total = (40 * hourPay) + (20 * (hourPay * 1.5)) + ((hourWork - 60) * (hourPay * 2));
+        cout << "Gross Pay = (40*" << hourPay << ") + (20 * (" << hourPay << "*1.5))" << " + ((" << hourWork << "-60) * (" << hourPay << "*2)) = $" << total << '\n';
+    }
+    double getTotal() const
+    {
+        return total;
+    }
+};
+
+class TaxableIncome : public GrossPay
+{
+private:
+    double ND0, taxTotal; // Number of Dependents (ND)
+
+public:
+    void setNoD(int num)
+    {
+        ND0 = num;
+    }
+
+    void calculateTax(double grossPayTotal)
+    {
+        if (ND0 == 1)
+        {
+            taxTotal = grossPayTotal * (1 - 0.04);
+            cout << "Taxable income = " << grossPayTotal << "* (1 - 0.04) = $" << getTaxTotal() << '\n';
+        }
+        else if (ND0 == 2)
+        {
+            taxTotal = grossPayTotal * (1 - 0.0775);
+            cout << "Taxable income = " << grossPayTotal << "* (1 - 0.0775) = $" << getTaxTotal() << '\n';
+        }
+        else if (ND0 == 3)
+        {
+            taxTotal = grossPayTotal * (1 - 0.1125);
+            cout << "Taxable income = " << grossPayTotal << "* (1 - 0.1125) = $" << getTaxTotal() << '\n';
+        }
+        else if (ND0 == 4)
+        {
+            taxTotal = grossPayTotal * (1 - 0.145);
+            cout << "Taxable income = " << grossPayTotal << "* (1 - 0.145) = $" << getTaxTotal() << '\n';
+        }
+        else if (ND0 == 5)
+        {
+            taxTotal = grossPayTotal * (1 - 0.175);
+            cout << "Taxable income = " << grossPayTotal << "* (1 - 0.175) = $" << getTaxTotal() << '\n';
+        }
+        else if (ND0 >= 6)
+        {
+            taxTotal = grossPayTotal * (1 - 0.2025);
+            cout << "Taxable income = " << grossPayTotal << "* (1 - 0.2025) = $" << getTaxTotal() << '\n';
+        }
+    }
+
+    double getTaxTotal()
+    {
+        return taxTotal;
     }
 };
 
@@ -44,31 +100,49 @@ int main()
 {
     double HP;
     double HW;
-    GrossPay GP;
+    int ND;
 
-    cout << "Hour Worked = ";
+    GrossPay GP;
+    TaxableIncome TaI;
+
+    cout << "Enter the hours worked: ";
     cin >> HW;
     GP.setHourWork(HW);
 
-    cout << "Hour Pay = ";
+    cout << "Enter the hourly pay: ";
     cin >> HP;
     GP.setHourPay(HP);
+
+    cout << "Enter the number of dependents: ";
+    cin >> ND;
+    TaI.setNoD(ND);
 
     if (HW > 0 && HW <= 40)
     {
         GP.lessThan40();
     }
-    else if (HW > 0 && HW > 40 && HW <= 60)
+    else if (HW > 40 && HW <= 60)
     {
         GP.between40and60();
     }
-    else if (HW > 0 && HW > 60)
+    else if (HW > 60)
     {
         GP.moreThan60();
     }
     else
     {
-        cout << "Sorry Hour Worked need to be positive number." << '\n';
+        cout << "Invalid number of Working Hours." << '\n';
+        return 1;
+    }
+
+    if (ND >= 0)
+    {
+        TaI.calculateTax(GP.getTotal());
+    }
+    else
+    {
+        cout << "Invalid number of dependents." << '\n';
+        return 1;
     }
 
     return 0;
